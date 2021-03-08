5ebc033995566da82056337939a37285142ec402
andreasnilsen <danduras@gmail.com>
2021-03-08 13:45:35 +0100

    run more often

--- src/main/kotlin/no/nav/syfo/altinn/narmesteleder/NarmesteLederDownloadService.kt
+++ src/main/kotlin/no/nav/syfo/altinn/narmesteleder/NarmesteLederDownloadService.kt
@@ -25,7 +25,7 @@ class NarmesteLederDownloadService(
     companion object {
         private const val SERVICE_CODE = "4596"
         private const val LANGUAGE_ID = 1033
-        private const val DELAY = 60_000L * 5
+        private const val DELAY = 60_000L * 1
     }
 
     suspend fun start() {
