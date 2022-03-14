460a130c94a12e50a4d97e74b0f996c9571bbb80
Andreas Nilsen <danduras@gmail.com>
2022-03-14 07:29:43 +0100

    update logging, add sykmeldingId

--- src/main/kotlin/no/nav/syfo/altinn/narmesteleder/NarmesteLederDownloadService.kt
+++ src/main/kotlin/no/nav/syfo/altinn/narmesteleder/NarmesteLederDownloadService.kt
@@ -46,7 +46,9 @@ class NarmesteLederDownloadService(
     private suspend fun pollDownloadQueueAndHandle() {
         try {
             val items = iDownloadQueueExternalBasic.getDownloadQueueItems(navUsername, navPassword, SERVICE_CODE)
-            log.info("Got itmes from download queue from altinn ${items.downloadQueueItemBE.size}")
+            if(items.downloadQueueItemBE.size > 0) {
+                log.info("Got itmes from download queue from altinn ${items.downloadQueueItemBE.size}")
+            }
             items.downloadQueueItemBE.forEach { handleDownloadItem(it) }
         } catch (ex: IDownloadQueueExternalBasicGetDownloadQueueItemsAltinnFaultFaultFaultMessage) {
             log.error("Error getting items from DownloadQueueu" + ex.faultInfo.altinnErrorMessage)
