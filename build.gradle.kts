dbfc061a314e0c715dc6664bb3741d8869082741
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-11-07 09:58:03 +0100

    chore: bumped ktor to 3.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ version = "1.0.0"
 val coroutinesVersion = "1.9.0"
 val jacksonVersion = "2.18.0"
 val kluentVersion = "1.73"
-val ktorVersion = "2.3.12"
+val ktorVersion = "3.0.1"
 val logbackVersion = "1.5.8"
 val logstashEncoderVersion = "8.0"
 val prometheusVersion = "0.16.0"
@@ -35,7 +35,7 @@ plugins {
     id("application")
     id("com.diffplug.spotless") version "6.25.0"
     kotlin("jvm") version "2.0.20"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.4"
 }
 
 application {
