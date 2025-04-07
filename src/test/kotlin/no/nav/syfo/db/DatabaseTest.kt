1d499a3831b09082f50cdb63725cc41dfaa6ecc3
Andreas Sagen Aspaas <danduras@gmail.com>
2025-04-07 15:50:53 +0200

    chore: update postgres version

--- src/test/kotlin/no/nav/syfo/db/DatabaseTest.kt
+++ src/test/kotlin/no/nav/syfo/db/DatabaseTest.kt
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.testcontainers.containers.PostgreSQLContainer
 
-class PsqlContainer : PostgreSQLContainer<PsqlContainer>("postgres:12.0")
+class PsqlContainer : PostgreSQLContainer<PsqlContainer>("postgres:14.0")
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class DatabaseTest {
