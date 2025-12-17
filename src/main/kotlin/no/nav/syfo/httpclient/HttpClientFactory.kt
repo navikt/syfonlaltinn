d225918e922197350c51dc5df2a50f66bad9d352
Halvor Grizzly Bjørn <mrbjoern92@gmail.com>
2025-12-17 13:52:57 +0100

    fix: add timeout and retry to http client to avoid app dying on failed http calls (#299)
    Co-authored-by: Karl J. Overå <k@rl.run>

--- src/main/kotlin/no/nav/syfo/httpclient/HttpClientFactory.kt
+++ src/main/kotlin/no/nav/syfo/httpclient/HttpClientFactory.kt
@@ -8,10 +8,15 @@ import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.engine.apache.ApacheEngineConfig
+import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.HttpResponseValidator
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.isSuccess
 import io.ktor.network.sockets.SocketTimeoutException
 import io.ktor.serialization.jackson.jackson
+import no.nav.syfo.securelog
 import javax.naming.ServiceUnavailableException
 
 class HttpClientFactory private constructor() {
@@ -25,6 +30,27 @@ class HttpClientFactory private constructor() {
                         configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
                         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                     }
+                }
+                install(HttpRequestRetry) {
+                    exponentialDelay(2.0, baseDelayMs = 1000, maxDelayMs = 20_000)
+                    retryOnExceptionIf(5) { request, throwable ->
+                        securelog.warn("Caught exception ${throwable.message}, for url ${request.url}")
+                        true
+                    }
+                    retryIf(5) { request, response -> !response.status.isSuccess() }
+                    retryIf(5) { _, response ->
+                        when {
+                            response.status == HttpStatusCode.Conflict -> false
+                            response.status.isSuccess() -> false
+                            else -> true
+                        }
+                    }
+                }
+
+                install(HttpTimeout) {
+                    connectTimeoutMillis = 40000
+                    requestTimeoutMillis = 40000
+                    socketTimeoutMillis = 40000
                 }
                 HttpResponseValidator {
                     handleResponseExceptionWithRequest { exception, _ ->
